f47613af756625d2802cbb0cf210479debfb948d
lucas persson <luben93@gmail.com>
2016-05-15 17:20:56 +0200

    location now reported

--- app/src/main/java/za/co/neilson/alarm/alert/AlarmAlertActivity.java
+++ app/src/main/java/za/co/neilson/alarm/alert/AlarmAlertActivity.java
@@ -14,17 +14,21 @@ package za.co.neilson.alarm.alert;
 import za.co.neilson.alarm.Alarm;
 import za.co.neilson.alarm.R;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.telephony.PhoneStateListener;
@@ -39,11 +43,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class AlarmAlertActivity extends Activity implements OnClickListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class AlarmAlertActivity extends Activity implements OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private Alarm alarm;
     private MediaPlayer mediaPlayer;
+    protected GoogleApiClient mGoogleApiClient;
 
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
     private StringBuilder answerBuilder = new StringBuilder();
 
     private MathProblem mathProblem;
@@ -57,6 +70,8 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        buildGoogleApiClient();
+
         super.onCreate(savedInstanceState);
         final Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
@@ -67,12 +82,10 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
         setContentView(R.layout.alarm_alert);
 
 
-
-
         Bundle bundle = this.getIntent().getExtras();
         alarm = (Alarm) bundle.getSerializable("alarm");
 
-        sendNotification(this,alarm.getAlarmName(),alarm);
+        sendNotification(this, alarm.getAlarmName(), alarm);
 
         this.setTitle(alarm.getAlarmName());
 
@@ -117,7 +130,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
         ((Button) findViewById(R.id.Button_alarm_off)).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                alarmActive=false;  //Toast.makeText(getApplicationContext(),"off",Toast.LENGTH_SHORT).show();
+                alarmActive = false;  //Toast.makeText(getApplicationContext(),"off",Toast.LENGTH_SHORT).show();
                 foo.finish();
             }
         });
@@ -165,14 +178,24 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
+        mGoogleApiClient.connect();
+
         alarmActive = true;
     }
 
-    public static int sendNotification(Context context,String str,Alarm alarm){
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i("locationalarm", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
 
-        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+    public static int sendNotification(Context context, String str, Alarm alarm) {
 
-        int mId=1;
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+
+        int mId = 1;
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
@@ -180,7 +203,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
                         .setContentTitle(context.getString(R.string.app_name))
                         .setContentText(str);
 // Creates an explicit intent for an Activity in your app
-        Intent resultIntent =         new Intent(context,context.getClass());
+        Intent resultIntent = new Intent(context, context.getClass());
         resultIntent.putExtra("alarm", alarm);
 // The stack builder object will contain an artificial back stack for the
 // started Activity.
@@ -270,8 +293,11 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
         } catch (Exception e) {
 
         }
-        NotificationManager nm=(NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager nm = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
         nm.cancelAll();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
         super.onDestroy();
     }
 
@@ -338,6 +364,51 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
             return false;
         }
         return correct;
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+
+            Log.i("locationalarm",  mLastLocation.getLatitude()+" "+mLastLocation.getLongitude());
+
+        } else {
+            Toast.makeText(this, "no_location_detected", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i("locationalarm", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
     }
 
 }
